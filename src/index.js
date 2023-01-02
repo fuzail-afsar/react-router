1356f1c421ab47c691f54eec0f3b9925f21bd17d
unknown <fuzailafsar92@gmail.com>
2023-01-02 16:36:01 +0500

    root route

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+import Root from "./routes/Root";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root />,
   },
 ]);
 
