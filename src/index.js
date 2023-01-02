92acce0881cc1f8370462f5207ec3f4e1a257376
unknown <fuzailafsar92@gmail.com>
2023-01-02 16:55:00 +0500

    contact route ui

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ErrorPage from "./ErrorPage";
 import "./index.css";
+import Contact from "./routes/Contact";
 import Root from "./routes/Root";
 
 const router = createBrowserRouter([
@@ -10,6 +11,10 @@ const router = createBrowserRouter([
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+  },
+  {
+    path: "/contacts/:contactId",
+    element: <Contact />,
   },
 ]);
 
